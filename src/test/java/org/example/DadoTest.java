fc59fcded1dbb3d66ebfe448d4093a7dea59db3a
rodrigo-cloureiro <rodrigo.cloureiro@al.infnet.edu.br>
2023-11-09 21:45:59 -0300

    DadoTest implementado

---
+++ src/test/java/org/example/DadoTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.example.personagens.Personagem;
+import org.example.util.Dado;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DadoTest {
+    @Test
+    @DisplayName("Deve testar os dados")
+    public void dado() {
+        assertTrue(Dado.rolarD2() > 0 && Dado.rolarD2() < 3);
+        assertTrue(Dado.rolarD4() > 0 && Dado.rolarD4() < 5);
+        assertTrue(Dado.rolarD6() > 0 && Dado.rolarD6() < 7);
+        assertTrue(Dado.rolarD8() > 0 && Dado.rolarD8() < 9);
+        assertTrue(Dado.rolarD10() > 0 && Dado.rolarD10() < 11);
+    }
+}
